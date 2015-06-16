8a081b4c633414ea71a883be732bcf6a0b662e87
Park ChulWoo <mozziluv@gmail.com>
2015-06-17 01:13:26 +0900

    Fix invalid templates

--- coco/dashboard/views.py
+++ coco/dashboard/views.py
@@ -6,22 +6,25 @@ from posts.models import Post, Tag
 
 @login_required
 def index(request):
-    context = {'posts': Post.objects.filter(is_active=True) }
+    context = {
+        'title': "메인",
+        'posts': Post.objects.filter(is_active=True)
+    }
     return render(request, 'dashboard/index.html', context)
 
 @login_required
 def tagged_posts(request, tag_name=""):
     context = {
-        'tag': tag_name,
+        'title': '#%s' % (tag_name),
         'posts': Post.objects.filter(tags__name=tag_name, is_active=True)
     }
-    return render(request, 'dashboard/search_result.html', context)
+    return render(request, 'dashboard/index.html', context)
 
 @login_required
 def users_posts(request, username=""):
     context = {
-        'username': username,
+        'title': '@%s' % (username),
         'posts': Post.objects.filter(user__username=username, is_active=True)
     }
 
-    return render(request, 'dashboard/search_result.html', context)
+    return render(request, 'dashboard/index.html', context)
