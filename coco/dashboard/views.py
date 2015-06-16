ce7c5986b5d255acbd360c79f609986c3099d996
Park ChulWoo <mozziluv@gmail.com>
2015-06-17 00:46:38 +0900

    Add search post by username

--- coco/dashboard/views.py
+++ coco/dashboard/views.py
@@ -6,13 +6,22 @@ from posts.models import Post, Tag
 
 @login_required
 def index(request):
-    context = {'posts': Post.objects.all()}
+    context = {'posts': Post.objects.filter(is_active=True) }
     return render(request, 'dashboard/index.html', context)
 
 @login_required
 def tagged_posts(request, tag_name=""):
     context = {
         'tag': tag_name,
-        'posts': Post.objects.filter(tags__name=tag_name)
+        'posts': Post.objects.filter(tags__name=tag_name, is_active=True)
     }
+    return render(request, 'dashboard/search_result.html', context)
+
+@login_required
+def users_posts(request, username=""):
+    context = {
+        'username': username,
+        'posts': Post.objects.filter(user__username=username, is_active=True)
+    }
+
     return render(request, 'dashboard/search_result.html', context)
